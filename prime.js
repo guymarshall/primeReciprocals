a1671b020e04ccc8d3dae108446ea299b4a29719
Guy Marshall <guymarshall9@gmail.com>
2023-04-02 21:30:36 +0100

    Add more efficient isPrime

--- prime.js
+++ prime.js
@@ -1,15 +1,19 @@
 const isPrime = input => {
-    if (input % 2 == 0) {
+    if (input < 2) {
         return false;
     }
-
-    for (let i = 3; i < input; i += 2) {
-        if (input % i == 0) {
-            return false;
+    if (input === 2) {
+        return true;
+    }
+    if (input % 2 === 0) {
+        return false;
+    }
+    for (let i = 3; i <= Math.sqrt(input); i += 2) {
+        if (input % i === 0) {
+        return false;
         }
     }
-
     return true;
-};
+};  
 
 module.exports = isPrime;
