716cfc2243352fd5946b8b83fb6b83bbdcfc2609
Guy Marshall <guymarshall9@gmail.com>
2023-04-02 21:54:31 +0100

    Add reciprocalDecimalCount to reciprocal.js

---
+++ reciprocal.js
@@ -0,0 +1,15 @@
+const reciprocalDecimalCount = (input) => {
+    let dividend = 1;
+    const remainders = new Map();
+    let decimalCount = 0;
+    
+    while (!remainders.has(dividend)) {
+        remainders.set(dividend, decimalCount);
+        dividend = (dividend * 10) % input;
+        decimalCount++;
+    }
+    
+    return decimalCount - remainders.get(dividend);
+}
+
+module.exports = reciprocalDecimalCount;
