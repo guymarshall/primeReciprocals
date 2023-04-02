ef89d5442f0034f59fdd504ae80d276cc241db22
Guy Marshall <guymarshall9@gmail.com>
2023-04-02 21:16:17 +0100

    Add prompt for user input

---
+++ app.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+
+const lineReader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+lineReader.question('Enter a number: ', (number) => {
+    console.log(`You entered: ${number}`);
+    lineReader.close();
+});
